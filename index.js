a80609452e85dc6a3c1cb860f4a7ffa75b1af9dc
Chad Gilchrist <chadly981@gmail.com>
2019-05-19 21:55:55 +1200

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-const express = require('express');
-const app = express();
-const path = require('path');
-const router = express.Router();
+var express = require("express");
+var app     = express();
+var path    = require("path");
 
-router.get('/',function(req,res){
-  res.sendFile(path.join(__dirname+'/index.html'));
-  //__dirname : It will resolve to your project folder.
+app.use(express.static(__dirname + '/view'));
+
+app.get('/',function(req,res){
+  res.sendFile((path.join(__dirname+'/index.html'));
 });
 
-app.use('/', router);
 app.listen(port);
