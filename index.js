dfa2adef02fdc96035643c2c47bf2278e73f06aa
Chad Gilchrist <chadly981@gmail.com>
2019-05-19 22:07:50 +1200

    updating links

--- index.js
+++ index.js
@@ -7,4 +7,8 @@ app.get("/", function(req, res){
   res.sendFile(__dirname + "/index.html");
 });
 
+app.get("/listings", function(req, res){
+  res.sendFile(__dirname + "/listings.html");
+});
+
 app.listen(port);
