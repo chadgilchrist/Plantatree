746d6e0f3b42ea3f83c39a9a5c030f4cd9fa0162
Rainingtacoz <42161763+Rainingtacoz@users.noreply.github.com>
2019-05-21 00:41:59 +1200

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
-var express = require("express");
+var express = require('express');
+var mysql = require('mysql');
 var app = express();
 var port = process.env.PORT || 3000;
 
 app.use(express.static('public'));
+
+var connection = mysql.createconncection({
+  host: 'us-cdbr-iron-east-02.cleardb.net',
+  user: 'b4d552e81ff3fc',
+  password: 'b3031d21',
+  database: 'heroku_fa55a69b1e6a26a'
+});
+
+app.get("/db", function(req, res){
+  connection.connect();
+
+  connection.query('SELECT * products', function(err, rows, fields)) {
+    if (err) throw err;
+
+
+
+
+  }
+});
 
 app.get("/", function(req, res){
   //res.send("Test");
@@ -16,5 +36,9 @@ app.get("/listings", function(req, res){
 app.get("/login", function(req, res){
   res.sendFile(__dirname + "/login.html");
 });
+
+app.get("/db", function(req, res)) {
+
+}
 
 app.listen(port);
