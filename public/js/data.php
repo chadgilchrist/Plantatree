0d13b859a99db25fed8207efb0afd5cc9e3e9af7
Chad Gilchrist <chadly981@gmail.com>
2019-05-22 01:16:42 +1200

    going back commit

--- public/js/data.php
+++ public/js/data.php
@@ -3,7 +3,7 @@
 <?php
 
 
-	$connection = mysqli_connect("us-cdbr-iron-east-02.cleardb.net", "b4d552e81ff3fc", "b3031d21", "heroku_fa55a69b1e6a26a");
+	$connection = mysqli_connect("cmslamp14.aut.ac.nz", "scj3411", "Lego9819", "scj3411");
 
 	if (!$connection) {
 		echo "Error: Unable to connect to MySQL." . PHP_EOL;
@@ -13,29 +13,28 @@
 		$name= $_POST['name'];
 		$pwd = $_POST['pwd'];
 
-		$query = "SELECT * FROM test;";
-		//$query = "SELECT * FROM test WHERE pwd = '".$pwd."' ";
+		$query = "SELECT * FROM labseven WHERE pwd = '".$pwd."' ";
 
 		$result = mysqli_query($connection, $query);
 
 		if(!$result) {
 			echo "<p>Something wrong with the query</p>";
 		} else {
-			if (mysqli_num_rows($result)==0) {
+			if (mysqli_num_rows($result)==0) { 
 				echo "<p>your account doesn't exist</p>";
 			} else {
 				echo "<table border=\"1\">";
 				echo "<tr>\n"
-					 ."<th scope=\"col\">ID</th>\n"
-				   ."<th scope=\"col\">First Name</th>\n"
-					 ."<th scope=\"col\">Last Name</th>\n"
+					 ."<th scope=\"col\">Name</th>\n"
+				   ."<th scope=\"col\">Password</th>\n"
+					 ."<th scope=\"col\">Email</th>\n"
 					 ."</tr>\n";
 
 				while ($row = mysqli_fetch_assoc($result)){
 					echo "<tr>";
-					echo "<td>",$row["id"],"</td>";
-					echo "<td>",$row["fname"],"</td>";
-					echo "<td>",$row["lname"],"</td>";
+					echo "<td>",$row["name"],"</td>";
+					echo "<td>",$row["pwd"],"</td>";
+					echo "<td>",$row["email"],"</td>";
 					echo "</tr>";
 				}
 			}
