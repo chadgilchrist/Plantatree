3c1e40534106a0d99fb3f60e1c886816a7438733
danyalacetin <42688885+danyalacetin@users.noreply.github.com>
2019-05-23 21:48:39 +1200

    More Database Functionality

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 var express = require("express");
 var app = express();
 var port = process.env.PORT || 3000;
-
-app.use(express.static('public'));
-
 var mysql = require('mysql');
 
 var connection = mysql.createConnection({
@@ -13,13 +10,27 @@ var connection = mysql.createConnection({
     database : 'heroku_c9cba001b0caa9d'
 });
 
+app.use(express.static(__dirname + '/public'));
+app.set('views', __dirname + '/public/trees');
+app.engine('html', require('ejs').renderFile);
+app.set("view engine", "html");
+
+
 connection.connect(function(err) {
     if (err) throw err;
 });
 
 app.get("/", function(req, res){
-  //res.send("Test");
   res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/tree", function(req, res){
+  connection.query('SELECT * FROM products WHERE product_id = ?', req.query.id, function(err, result) {
+    if (err) throw err;
+    console.log(result);
+    res.render(__dirname + "/tree1.html", {id: req.query.id, name: result[0].product_name});
+  });
+
 });
 
 app.get("/listings", function(req, res){
@@ -35,12 +46,12 @@ app.get("/cart", function(req, res){
 });
 
 app.get('/test',function(req,res){
-    connection.query('SELECT * FROM test', function(err, result) {
+    connection.query('SELECT * FROM products', function(err, result) {
       if (err) throw err;
       console.log(result);
       res.send(result);
     });
 });
 
-
 app.listen(port);
+console.log(`Example app listening on port ${port}!`)
