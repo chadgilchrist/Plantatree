c890d203106fde0d035cf816af3b5295191603a5
danyalacetin <42688885+danyalacetin@users.noreply.github.com>
2019-05-23 20:22:41 +1200

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,19 @@ var port = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    host     : 'us-cdbr-iron-east-02.cleardb.net',
+    user     : 'b44ace26fea6f3',
+    password : 'ea11b339',
+    database : 'heroku_c9cba001b0caa9d'
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+});
+
 app.get("/", function(req, res){
   //res.send("Test");
   res.sendFile(__dirname + "/index.html");
@@ -19,6 +32,14 @@ app.get("/login", function(req, res){
 
 app.get("/cart", function(req, res){
   res.sendFile(__dirname + "/cart.html");
+});
+
+app.get('/test',function(req,res){
+    db.query('SELECT * FROM products', function(err, result) {
+      if (err) throw err;
+      //console.log(result);
+      res.send(result);
+    });
 });
 
 
