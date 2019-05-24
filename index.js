6cda3705e9bc833334580c80c66b0df45369b6f6
danyalacetin <42688885+danyalacetin@users.noreply.github.com>
2019-05-24 23:47:46 +1200

    Fixing db problems

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ var app = express();
 var port = process.env.PORT || 3000;
 var mysql = require('mysql');
 
-var connection = mysql.createConnection({
-    host     : 'us-cdbr-iron-east-02.cleardb.net',
-    user     : 'b44ace26fea6f3',
-    password : 'ea11b339',
-    database : 'heroku_c9cba001b0caa9d'
-});
+var db_config = {
+  host     : 'us-cdbr-iron-east-02.cleardb.net',
+  user     : 'b44ace26fea6f3',
+  password : 'ea11b339',
+  database : 'heroku_c9cba001b0caa9d'
+}
+
 
 app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/public/trees');
@@ -16,23 +17,19 @@ app.engine('html', require('ejs').renderFile);
 app.set("view engine", "html");
 
 
-connection.connect(function(err) {
-    if (err) throw err;
-});
-
 app.get("/", function(req, res){
   res.sendFile(__dirname + "/index.html");
 });
 
 app.get("/tree", function(req, res){
+  var connection = mysql.createConnection(db_config);
+  connection.connect(function(err) {if (err) throw err;});
   connection.query('SELECT * FROM products WHERE product_id = ?', req.query.id, function(err, result) {
     if (err) throw err;
     console.log(result);
     res.render(__dirname + "/tree1.html", {
         id: req.query.id,
         result: result[0]
-
-
         // name: result[0].product_name,
         // description: result[0].description,
         // price: result[0].price,
@@ -41,9 +38,12 @@ app.get("/tree", function(req, res){
         // height: result[0].height
       });
   });
+  connection.end();
 });
 
 app.get("/search", function(req, res){
+  var connection = mysql.createConnection(db_config);
+  connection.connect(function(err) {if (err) throw err;});
   connection.query('SELECT * FROM products WHERE product_id = ?', req.query.id, function(err, result) {
     if (err) throw err;
     console.log(result);
@@ -52,10 +52,12 @@ app.get("/search", function(req, res){
         name: result[0].product_name
       });
   });
+  connection.end();
 }); // listingsserach page then do results 0, 1, 2 etc
 
 app.get("/listings", function(req, res){
-  // res.sendFile(__dirname + "/listings.html");
+  var connection = mysql.createConnection(db_config);
+  connection.connect(function(err) {if (err) throw err;});
   connection.query('SELECT * FROM products', function(err, result) {
     if (err) throw err;
     console.log(result);
@@ -63,6 +65,7 @@ app.get("/listings", function(req, res){
         products: result
       });
   });
+  connection.end();
 });
 
 app.get("/login", function(req, res){
@@ -74,11 +77,14 @@ app.get("/cart", function(req, res){
 });
 
 app.get('/test',function(req,res){
+    var connection = mysql.createConnection(db_config);
+    connection.connect(function(err) {if (err) throw err;});
     connection.query('SELECT * FROM products', function(err, result) {
       if (err) throw err;
       console.log(result);
       res.send(result);
     });
+    connection.end();
 });
 
 app.listen(port);
